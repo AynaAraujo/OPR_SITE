c0fea7b7ce11d58b41d790bea7a7689bc5d141d2
ayna <ayna.araujo@upe.br>
2025-11-24 08:55:24 -0300

    criando back branch

--- opr/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <p>© {new Date().getFullYear()} Orquestra Popular do Recife — Todos os direitos reservados.</p>
-    </footer>
-  )
-}
--- opr/src/pages/Galeria.jsx
+++
@@ -1,8 +0,0 @@
-export default function Galeria() {
-  return (
-    <section className="galeria">
-      <h2>Galeria Multimídia</h2>
-      <p>Em breve: fotos, vídeos e momentos marcantes da OPR!</p>
-    </section>
-  )
-}
